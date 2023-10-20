88a2bc492e70d760a58fa9808e31a7cd2a3e0d15
KimHaKyeong <rlagkrud32@naver.com>
2023-10-20 18:44:27 +0900

    Star Field 5일차

---
+++ waapi-group/Star.js
@@ -0,0 +1,9 @@
+﻿export default class Star {
+    constructor(number) {
+        this.element = document.createElement('div');
+        this.element.classList.add('star')
+        this.element.innerHTML = number;
+    
+        document.body.append(this.element);
+    }
+}
