61186088046581262f66d282aae5cf1fe6c1806a
KimHaKyeong <rlagkrud32@naver.com>
2023-10-26 01:28:21 +0900

    EarthAnimation

---
+++ SelfAnimation/Star.js
@@ -0,0 +1,11 @@
+﻿/*여기서 별을 만들어서 바디에 조립*/
+export default class Star {
+    constructor(number) {
+        this.element = document.createElement('div');
+        /*스타 클래스 적용*/
+        this. element.classList.add('star');
+        this. element.innerHTML = number;
+       
+        document.body.append(this.element);
+    }
+}
